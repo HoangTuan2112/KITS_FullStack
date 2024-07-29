3f39ad2aeb54817c0dc2961d5e6729d569762556
Nguyen Hoang Tuan <tuanmaiyeunguoido@gmail.com>
2024-07-29 16:52:13 +0700

    buoi 22 v2

--- reactjs/todoredux/src/component/ToDoDisplay.js
+++ reactjs/todoredux/src/component/ToDoDisplay.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addToDo, deleteToDo } from "../redux/todoReducer";
+import { addToDo, checkToDo, deleteToDo } from "../redux/todoReducer";
 
 export default function ToDoDisplay() {
   const list = useSelector((state) => state.todoReducer.students);
   const dispatch = useDispatch();
   const [text, setText] = React.useState("");
+  const [edit, setEdit] = React.useState(true);
+  const inputRef = React.useRef(null);
   console.log(list);
 
   return (
@@ -23,8 +25,9 @@ export default function ToDoDisplay() {
       />
       {list.map((todo) => (
         <div key={todo.id}>
-          <input type="checkbox" checked={todo.check}  />
-          {todo.job}
+          <input type="checkbox" checked={todo.check}   />
+
+        <input ref={inputRef} placeholder={todo.job} readOnly onClick={()=>dispatch(checkToDo(todo.id))} onDoubleClick={()=>{inputRef.current}}></input>
           <button onClick={() => dispatch(deleteToDo(todo.id))}>delete</button>
         </div>
       ))}
